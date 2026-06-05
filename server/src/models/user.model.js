e7855f1668e4c473a6e9c24d74a788a9e842f236
fardeen077 <fardeen36143@gmail.com>
2026-06-05 09:46:32 +0530

    Add user model with authentication methods and schema validation

---
+++ server/src/models/user.model.js
@@ -0,0 +1,69 @@
+import jwt, { sign } from "jsonwebtoken"
+import bcrypt from "bcryptjs"
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            "Please enter a valid email",
+        ],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    refreshToken: {
+        type: String,
+        default: "",
+    },
+    profileImage: {
+        type: String,
+        default: "https://cdn-icons-png.flaticon.com/512/847/847969.png",
+    }
+}, { timestamps: true });
+
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.isPasswordCorrect = async function () {
+    return await bcrypt.compare(password, this.password)
+};
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        });
+};
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id,
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        });
+};
+
+export const User = mongoose.model("User", userSchema);
