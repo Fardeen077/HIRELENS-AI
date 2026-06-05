d7284e33dff8c75df6ec12a1f8e9c5ca9ca72c20
fardeen077 <fardeen36143@gmail.com>
2026-06-05 10:14:38 +0530

    Add role-based access control middleware

---
+++ server/src/middleware/role.middleware.js
@@ -0,0 +1,21 @@
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+
+export const roles = (...allowedRoles) => {
+    return asyncHandler(async (req, res, next) => {
+        if (!req.user) {
+            throw new ApiError(401, "Authentication required")
+        }
+        const userRole = req.user.role;
+
+        if (userRole) {
+            throw new ApiError(403, "user role not found");
+        }
+
+        if (!allowedRoles.includes(userRole)) {
+            throw new ApiError(403, "You are not allowed to access this resource");
+        }
+        next();
+    })
+}
